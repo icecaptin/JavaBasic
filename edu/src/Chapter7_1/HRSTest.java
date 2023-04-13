309fc5f1d40913a1a3ea8d21e0f846ccc22c173f
Icecaptin <tjdqn0228@gmail.com>
2023-04-13 09:29:02 +0900

    20230413_1

--- edu/src/Chapter7_1/HRSTest.java
+++ edu/src/Chapter7_1/HRSTest.java
@@ -1,64 +1,82 @@
 package Chapter7_1;
 
-abstract class Employee {
+abstract class Employee { 
 	String name;
-	int salary = 8720;
-//	int saled = 3000;
-//	int consulting = 2000;
-//	int teamsalary = 5000;
+	int salary;
+
+	public Employee(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
 	
-	public abstract void calcSalary();
+	public abstract void calcSalary(); //부모매서드 /*abstract를 붙이면 추상 : 메소드의 바디를 못가짐 ({~~})*/ //메소드의 바디가 없는 경우
+
 	public abstract void calcBonus();
 }
 
 class Salesman extends Employee {
-	public void calcSalary() {
-		System.out.println("세일즈맨 급여 = 기본급 + 판매수당 = ");
+	int sale;
+	
+	public Salesman(String name, int salary, int sale) {
+		super(name, salary);
+		this.sale = sale;	
+	}
+	
+	public void calcSalary () { //부모매서드를 그대로 상속받아 작성.
+		System.out.println(name + "급여 = " + salary + sale );
 	}
 
 	public void calcBonus() {
-		System.out.println("세일즈맨 보너스 = 기본급 * 12 * 4 = ");
+		System.out.println(name + " 보너스 = " + (salary * 12 * 4));
 	}
 }
 
 class Consultant extends Employee {
+	public Consultant(String name, int salary, int sale) {
+		super(name, salary);
+	}
+
 	public void calcSalary() {
-		System.out.println("컨설턴트 급여 = 기본급 + 컨설팅 수당 = ");
+		System.out.println(name + "급여 = " + salary + "컨설팅 특별 수당");
 	}
 
 	public void calcBonus() {
-		System.out.println("컨설턴트 보너스 = 기본급 * 12 * 2 = ");
+		System.out.println(name + " 보너스 = " + (salary * 12 * 2));
 	}
 }
-class Manager extends Employee {
+
+abstract class Manager extends Employee { //Employee로부터 상속받음.
+	public Manager(String name, int salary, int sale) {
+		super(name, salary);
+	}
+
 	public void calcSalary() {
-		System.out.println("매니저 = 기본급 + 팀 성과 수당 = ");
+		System.out.println("Manager 급여 = 기본급 + 팀 성과 수당");
+	}
+
+}
+
+class Director extends Manager { //매니저로부터(부모클래스) 상속받아서 calcSalary를 굳이 구현하지 않아도 자동적으로 구현이 된다. 그래서 일반클래스가 되고 밑에서 new~라고 선언이 가능함.
+	public Director(String name, int salary, int sale) {
+		super(name, salary, sale);
+		// TODO Auto-generated constructor stub
 	}
 
 	public void calcBonus() {
-		System.out.println("매니저 보너스 = 기본급 * 12 * 6");
+		System.out.println("Director 보너스 = 기본급 * 12 * 6");
 	}
 }
 
-
-class/*abstract*/ Director extends Manager {
-	public void calcSalary() {
-		System.out.println("디렉터 = 기본급 + 컨설팅 수당");
-	}
-	public void calcBonus() {
-		System.out.println("디렉터 보너스 = 기본급 * 12 * 6");
-	}
-}
 public class HRSTest {
-	public static void main(String[] args) {
-		Salesman sm = new Salesman();
-		Consultant cst = new Consultant();
-		Director dir = new Director();
-		Manager mg = new Manager();
+	public static void main(String[] args) {//추상클래스가 아니라 일반클래스라 인스턴스 선언 가능.
+		Salesman sm = new Salesman("Slaesman", 100 , 0);
+		Consultant con = new Consultant("Consultant", 0, 0);
+		Director dir = new Director("Director", 0, 0);
 		
-		sm.calcBonus();
-		cst.calcBonus();
-		dir.calcBonus();
-		mg.calcBonus();
+		sm.calcSalary();
+		con.calcSalary();
+		dir.calcSalary();
+		
 	}
+	
 }
--- edu/src/Chapter7_1/Mission.java
+++ edu/src/Chapter7_1/Mission.java
@@ -2,20 +2,17 @@ package Chapter7_1;
 
 public class Mission {
 	public static void main(String[] args) {
-		int price = 254454;
-		int fiftythousands = 50000;
-		int tenthousands = 10000;
-		int fivethousands = 5000;
-		int thousands = 1000;
+		int price = 327540;
+		int fiftythousands = price % 50000;
+		int tenthousands = price % 10000;
+		int fivethousands = price % 5000;
+		int thousands = price % 1000;
 		
+		System.out.println("5만원권 갯수 : " + (price /50000));
+		System.out.println("1만원권 갯수 : " + (fiftythousands /10000));
+		System.out.println("5천원권 갯수 : " + (tenthousands /5000));
+		System.out.println("1천원권 갯수 : " + (fivethousands /1000));
 		
-		
-		int result1 = (price / fiftythousands);
-		int result1_1 = (price - (result1 *50000)); 
-		System.out.println(result1_1);
-		int result2 = (result1_1 / tenthousands);
-		int result2_1 = (result1_1 - (result2 * 10000));
-		System.out.println(result2_1 - (result2 * 10000));
 	
 	}
 }
